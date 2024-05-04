4c4051800dd3ae76d9b4748f7715d04dee6f59c4
tochnonvp <tochnonvp@gmail.com>
2024-05-04 22:16:57 +0300

    message

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controllers/HelloController.java
@@ -0,0 +1,2 @@
+package ru.itmentor.spring.boot_security.demo.controllers;public class HelloController {
+}
---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/models/Person.java
@@ -0,0 +1,87 @@
+package ak.spreeng.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "password")
+    private String password;
+
+
+
+    public Person() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Person(String name, String surname, int age, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
